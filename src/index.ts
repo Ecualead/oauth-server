9e4f4ddd97b019547e28c5985f107f25f350ded7
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-04-12 23:58:26 -0500

    Allow auth token request if settings are settled

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@
  * @Project: IKOABO Auth Microservice API
  * @Filename: index.ts
  * @Last modified by:   millo
- * @Last modified time: 2020-04-05T22:56:31-05:00
+ * @Last modified time: 2020-04-12T23:57:42-05:00
  * @Copyright: Copyright 2020 IKOA Business Opportunity
  */
 
 import { Settings } from './controllers/Settings';
 import { ClusterServer, Logger } from '@ikoabo/core_srv';
+import { Authenticator } from '@ikoabo/auth_srv';
 import { AccountsProject } from './controllers/AccountsProject';
 import DomainRouter from './routers/v1/domains';
 import ProjectRouter from './routers/v1/projects';
@@ -18,31 +19,37 @@ import ApplicationRouter from './routers/v1/applications';
 import AccountRouter from './routers/v1/accounts';
 import OAuth2Router from './routers/v1/oauth2';
 
-/* Initialize cluster server */
-const clusterServer = ClusterServer.setup(Settings, { postMongo: initializeProjects });
+Authenticator.shared.setup(Settings.AUTH.SERVER);
+Authenticator.shared.authService(Settings.AUTH.ID, Settings.AUTH.SECRET)
+  .then(() => { })
+  .catch(() => { })
+  .finally(() => {
+    /* Initialize cluster server */
+    const clusterServer = ClusterServer.setup(Settings, { postMongo: initializeProjects });
 
-/* Initialize the logger */
-const logger = new Logger('AuthApp');
+    /* Initialize the logger */
+    const logger = new Logger('AuthApp');
 
-/**
- * Initialize projects custom profiles
- */
-function initializeProjects(): Promise<void> {
-  return new Promise<void>((resolve) => {
-    AccountsProject.shared.initialize()
-      .then(() => {
-        resolve();
-      }).catch((err: any) => {
-        logger.error('Error initializing projects', err);
-        resolve();
+    /**
+     * Initialize projects custom profiles
+     */
+    function initializeProjects(): Promise<void> {
+      return new Promise<void>((resolve) => {
+        AccountsProject.shared.initialize()
+          .then(() => {
+            resolve();
+          }).catch((err: any) => {
+            logger.error('Error initializing projects', err);
+            resolve();
+          });
       });
-  });
-}
+    }
 
-/* Run cluster with base routes */
-clusterServer.run({
-  '/v1/domain': DomainRouter,
-  '/v1/project': ProjectRouter,
-  '/v1/application': ApplicationRouter,
-  '/v1/oauth': [AccountRouter, OAuth2Router],
-});
+    /* Run cluster with base routes */
+    clusterServer.run({
+      '/v1/domain': DomainRouter,
+      '/v1/project': ProjectRouter,
+      '/v1/application': ApplicationRouter,
+      '/v1/oauth': [AccountRouter, OAuth2Router],
+    });
+  })
