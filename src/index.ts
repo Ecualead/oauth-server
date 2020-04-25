62544c9171d48119828ab55d4c03323fa3de483a
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-04-25 05:56:19 -0500

    Update initialization

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@
  * @Project: IKOABO Auth Microservice API
  * @Filename: index.ts
  * @Last modified by:   millo
- * @Last modified time: 2020-04-13T03:28:42-05:00
+ * @Last modified time: 2020-04-25T05:55:51-05:00
  * @Copyright: Copyright 2020 IKOA Business Opportunity
  */
 
 import { Settings } from './controllers/Settings';
-import { ClusterServer, Logger } from '@ikoabo/core_srv';
+import { ClusterServer } from '@ikoabo/core_srv';
 import { Authenticator } from '@ikoabo/auth_srv';
 import { Mail } from '@ikoabo/comm_srv';
 import { AccountsProject } from './controllers/AccountsProject';
@@ -20,40 +20,47 @@ import ApplicationRouter from './routers/v1/applications';
 import AccountRouter from './routers/v1/accounts';
 import OAuth2Router from './routers/v1/oauth2';
 
-Authenticator.shared.setup(Settings.AUTH.SERVER);
-Authenticator.shared.authService(Settings.AUTH.ID, Settings.AUTH.SECRET)
-  .then(() => { })
-  .catch(() => { })
-  .finally(() => {
-    /* Initialize mail component */
-    Mail.shared.setup(Settings.NOTIFICATIONS.SERVER, Authenticator.shared.token);
-
-    /* Initialize cluster server */
-    const clusterServer = ClusterServer.setup(Settings, { postMongo: initializeProjects });
-
-    /* Initialize the logger */
-    const logger = new Logger('AuthApp');
-
-    /**
-     * Initialize projects custom profiles
-     */
-    function initializeProjects(): Promise<void> {
-      return new Promise<void>((resolve) => {
-        AccountsProject.shared.initialize()
-          .then(() => {
-            resolve();
-          }).catch((err: any) => {
-            logger.error('Error initializing projects', err);
-            resolve();
-          });
+/**
+ * Initialize projects custom profiles
+ */
+function initializeProjects(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    AccountsProject.shared.initialize()
+      .then(() => {
+      }).catch((err: any) => {
+        console.error(err);
+      }).finally(() => {
+        resolve();
       });
-    }
+  });
+}
 
-    /* Run cluster with base routes */
-    clusterServer.run({
-      '/v1/domain': DomainRouter,
-      '/v1/project': ProjectRouter,
-      '/v1/application': ApplicationRouter,
-      '/v1/oauth': [AccountRouter, OAuth2Router],
-    });
-  })
+/**
+ * Authenticate agains auth service
+ */
+function requestCredentials(): Promise<void> {
+  return new Promise<void>((resolve) => {
+    Authenticator.shared.setup(Settings.AUTH.SERVER);
+    Authenticator.shared.authService(Settings.AUTH.ID, Settings.AUTH.SECRET)
+      .then(() => { })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        /* Initialize mail component */
+        Mail.shared.setup(Settings.NOTIFICATIONS.SERVER, Authenticator.shared.token);
+        resolve();
+      });
+  });
+}
+
+/* Initialize cluster server */
+const clusterServer = ClusterServer.setup(Settings, { preMongo: requestCredentials, postMongo: initializeProjects });
+
+/* Run cluster with base routes */
+clusterServer.run({
+  '/v1/domain': DomainRouter,
+  '/v1/project': ProjectRouter,
+  '/v1/application': ApplicationRouter,
+  '/v1/oauth': [AccountRouter, OAuth2Router],
+});
