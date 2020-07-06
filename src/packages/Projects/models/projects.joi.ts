fd587a10fd160f4aadc36f5f530a41b61345220a
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-07-05 22:46:41 -0500

    Updating project validations
    * Adding validations for project settings
    
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/packages/Projects/models/projects.joi.ts
+++ src/packages/Projects/models/projects.joi.ts
@@ -1,4 +1,5 @@
 import { Joi } from "@ikoabo/core_srv";
+import { PROJECT_LIFETIME_TYPES, PROJECT_RECOVER_TYPE, PROJECT_EMAIL_CONFIRMATION, SOCIAL_NETWORK_TYPES, NOTIFICATION_TYPES, PROFILE_FIELD_TYPES } from "./projects.enum";
 
 const ProjectLinks = Joi.object()
   .optional()
@@ -33,3 +34,78 @@ export const ProjectUpdateValidation = Joi.object().keys({
   image: Joi.string().allow("").optional(),
   links: ProjectLinks,
 });
+
+export const TypeSettingParamsValidation = Joi.object().keys({
+  id: Joi.objectId().required(),
+  type: Joi.number().required(),
+});
+
+export const ProfileFieldParamsValidation = Joi.object().keys({
+  id: Joi.objectId().required(),
+  name: Joi.string().required(),
+});
+
+export const SocialNetworkSettingValidation = Joi.object().keys({
+  type: Joi.number().default(SOCIAL_NETWORK_TYPES.SN_UNKNOWN).required(),
+  clientId: Joi.string().required(),
+  clientSecret: Joi.string().required(),
+  scope: Joi.string().allow("").optional(),
+  profile: Joi.array().items(Joi.string()).optional(),
+  profileMap: Joi.array().items(Joi.object().keys({
+    key: Joi.string().required(),
+    fields: Joi.array().items(Joi.string()).required()
+  })).optional(),
+  description: Joi.string().required(),
+});
+
+export const TokenLifetimeValidation = Joi.object().keys({
+  accessToken: Joi.number().default(PROJECT_LIFETIME_TYPES.LT_ONE_MONTH).required(),
+  refreshToken: Joi.number().default(PROJECT_LIFETIME_TYPES.LT_ONE_YEAR).required(),
+});
+
+export const RecoverTypeValidation = Joi.object().keys({
+  recover: Joi.number().default(PROJECT_RECOVER_TYPE.RT_DISABLED).required(),
+});
+
+export const RestrictIpValidation = Joi.object().keys({
+  ipAddress: Joi.string().required(),
+});
+
+export const EmailConfirmationValidation = Joi.object().keys({
+  type: Joi.number().default(PROJECT_EMAIL_CONFIRMATION.EC_CONFIRMATION_NOT_REQUIRED).required(),
+  time: Joi.number().default(PROJECT_LIFETIME_TYPES.LT_ONE_MONTH).required(),
+});
+
+export const PasswordPolicyValidation = Joi.object().keys({
+  len: Joi.number().default(5).required(),
+  upperCase: Joi.boolean().default(true).required(),
+  lowerCase: Joi.boolean().default(true).required(),
+  specialChars: Joi.boolean().default(false).required(),
+  numbers: Joi.boolean().default(true).required(),
+});
+
+export const NotificationsSettingsValidation = Joi.object().keys({
+  type: Joi.number().default(NOTIFICATION_TYPES.NT_UNKNOWN).required(),
+  signup: Joi.boolean().default(false).required(),
+  confirm: Joi.boolean().default(false).required(),
+  signin: Joi.boolean().default(false).required(),
+  chPwd: Joi.boolean().default(false).required(),
+  recover: Joi.boolean().default(false).required(),
+  urls: Joi.object().keys({
+    confirm: Joi.string().allow("").optional(),
+    recover: Joi.string().allow("").optional()
+  }).optional()
+});
+
+export const ProfileFieldSettingsValidation = Joi.object().keys({
+  name: Joi.string().required(),
+  description: Joi.string().allow("").optional(),
+  type: Joi.number().default(PROFILE_FIELD_TYPES.PF_UNKNOWN).required(),
+  defaultValue: Joi.string().allow("").optional(),
+  required: Joi.boolean().default(false)
+});
+
+export const ProfileFieldIndexSettingsValidation = Joi.object().keys({
+  names: Joi.array().items(Joi.string()).required()
+});
+
