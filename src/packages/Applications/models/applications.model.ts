7e776e29189ab4fbf5ff13234baaf3479bc8f4a0
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-07-08 03:25:47 -0500

    Adding application token type
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/packages/Applications/models/applications.model.ts
+++ src/packages/Applications/models/applications.model.ts
@@ -11,7 +11,8 @@ import {
 import { APPLICATION_TYPES } from "@/Applications/models/applications.enum";
 import { Project } from "@/Projects/models/projects.model";
 import { Client } from "oauth2-server";
-import { PROJECT_LIFETIME_TYPES } from "@/packages/Projects/models/projects.enum";
+import { PROJECT_LIFETIME_TYPES } from "@/Projects/models/projects.enum";
+import { OAUTH2_TOKEN_TYPE } from "@/OAuth2/models/oauth2.enum";
 
 @pre<Application>("save", function (next) {
   const obj: any = this;
@@ -32,7 +33,11 @@ export class Application extends BaseModel {
   @prop()
   description?: string;
 
-  @prop({ enum: APPLICATION_TYPES, required: true, default: APPLICATION_TYPES.APP_UNKNOWN })
+  @prop({
+    enum: APPLICATION_TYPES,
+    required: true,
+    default: APPLICATION_TYPES.APP_UNKNOWN,
+  })
   type?: APPLICATION_TYPES;
 
   @prop({ required: true, ref: Project })
@@ -90,9 +95,21 @@ export class Application extends BaseModel {
     const obj: any = this;
     let client = {
       id: obj.id,
+      type: OAUTH2_TOKEN_TYPE.TT_APPLICATION,
       grants: obj.grants,
-      accessTokenLifetime: obj.type === APPLICATION_TYPES.APP_SERVICE ? -1 : Objects.get(obj, 'project.settings.tokenLifetime.accessToken', PROJECT_LIFETIME_TYPES.LT_ONE_MONTH),
-      refreshTokenLifetime: Objects.get(obj, 'project.settings.tokenLifetime.refreshToken', PROJECT_LIFETIME_TYPES.LT_ONE_YEAR),
+      accessTokenLifetime:
+        obj.type === APPLICATION_TYPES.APP_SERVICE
+          ? -1
+          : Objects.get(
+              obj,
+              "project.settings.tokenLifetime.accessToken",
+              PROJECT_LIFETIME_TYPES.LT_ONE_MONTH
+            ),
+      refreshTokenLifetime: Objects.get(
+        obj,
+        "project.settings.tokenLifetime.refreshToken",
+        PROJECT_LIFETIME_TYPES.LT_ONE_YEAR
+      ),
       scope: obj.scope,
       project: obj.project,
     };
