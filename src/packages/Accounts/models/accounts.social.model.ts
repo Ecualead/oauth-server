c3cd3d79864bd92de31a2ac5f33e3b8b79d62c3d
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-05 17:18:12 -0500

    Update account social profile
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/packages/Accounts/models/accounts.social.model.ts
+++ src/packages/Accounts/models/accounts.social.model.ts
@@ -8,7 +8,7 @@
  * permission of the author.
  */
 import { prop, index } from "@typegoose/typegoose";
-import { ACCOUNT_SOCIAL_NETWORKS } from "@/Accounts/models/accounts.enum";
+import { SOCIAL_NETWORK_TYPES } from "@/Projects/models/projects.enum";
 
 @index({ type: 1 })
 @index({ socialId: 1 })
@@ -16,7 +16,7 @@ import { ACCOUNT_SOCIAL_NETWORKS } from "@/Accounts/models/accounts.enum";
 @index({ refreshToken: 1 })
 @index({ socialId: 1, accessToken: 1 })
 export class AccountSocialCredential {
-  @prop({ required: true, default: ACCOUNT_SOCIAL_NETWORKS.SN_UNKNOWN })
+  @prop({ required: true, default: SOCIAL_NETWORK_TYPES.SN_UNKNOWN })
   type!: number;
 
   @prop({ required: true })
@@ -27,6 +27,43 @@ export class AccountSocialCredential {
 
   @prop()
   refreshToken?: string;
+}
+
+export function socialNetworkToInt(social: string): SOCIAL_NETWORK_TYPES {
+  social = social.toLowerCase();
+  switch (social) {
+    case "facebook":
+      return SOCIAL_NETWORK_TYPES.SN_FACEBOOK;
+    case "google":
+      return SOCIAL_NETWORK_TYPES.SN_GOOGLE;
+    case "instagram":
+      return SOCIAL_NETWORK_TYPES.SN_INSTAGRAM;
+    case "linkedin":
+      return SOCIAL_NETWORK_TYPES.SN_LINKEDIN;
+    case "twitter":
+      return SOCIAL_NETWORK_TYPES.SN_TWITTER;
+    case "yahoo":
+      return SOCIAL_NETWORK_TYPES.SN_YAHOO;
+  }
+  return SOCIAL_NETWORK_TYPES.SN_UNKNOWN;
+}
+
+export function socialNetworkToStr(social: SOCIAL_NETWORK_TYPES): string {
+  switch (social) {
+    case SOCIAL_NETWORK_TYPES.SN_FACEBOOK:
+      return "facebook";
+    case SOCIAL_NETWORK_TYPES.SN_GOOGLE:
+      return "google";
+    case SOCIAL_NETWORK_TYPES.SN_INSTAGRAM:
+      return "instagram";
+    case SOCIAL_NETWORK_TYPES.SN_LINKEDIN:
+      return "linkedin";
+    case SOCIAL_NETWORK_TYPES.SN_TWITTER:
+      return "twitter";
+    case SOCIAL_NETWORK_TYPES.SN_YAHOO:
+      return "yahoo";
+  }
+  return null;
 }
 
 /* 
