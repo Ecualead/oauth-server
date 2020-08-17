c254884d7df0c7d420046233a2473af04cfc7df8
Reinier Millo Sánchez <millo@ikoabo.com>
2020-08-17 09:17:31 -0500

    Updating base validations
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

--- src/models/base.joi.ts
+++ src/models/base.joi.ts
@@ -3,21 +3,22 @@
  * All Rights Reserved
  * Author: Reinier Millo Sánchez <millo@ikoabo.com>
  *
- * This file is part of the IKOA Business Opportunity Auth Service.
+ * This file is part of the IKOA Business Opportunity
+ * Identity Management Service.
  * It can't be copied and/or distributed without the express
  * permission of the author.
  */
-import { Joi } from "@ikoabo/core_srv";
+import { Joi } from "@ikoabo/server";
 
 export const StatusValidation = Joi.object().keys({
   id: Joi.objectId().required(),
-  action: Joi.string().valid("enable", "disable").required(),
+  action: Joi.string().valid("enable", "disable").required()
 });
 
 export const ScopeValidation = Joi.object().keys({
-  scope: Joi.string().required(),
+  scope: Joi.string().required()
 });
 
 export const RestrictionValidation = Joi.object().keys({
-  restriction: Joi.string().required(),
+  restriction: Joi.string().required()
 });
