1fd3cc3aace04c0109f347175661735ff1c3c2ce
Reinier Millo Sánchez <millo@ikoabo.com>
2021-06-18 00:00:38 -0500

    Update main entry point
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import AsyncLock from "async-lock";
 import AccountRouter from "@/routers/v1/account.router";
 import ApplicationRouter from "@/routers/v1/application.router";
 import DomainRouter from "@/routers/v1/domain.router";
+import OAuth2ValidateRouter from "@/routers/v1/oauth2-validate.router";
 import OAuth2Router from "@/routers/v1/oauth2.router";
 import ExternalAuthRouter from "@/routers/v1/external-auth.router";
 import ProjectRouter from "@/routers/v1/project/project.router";
@@ -38,7 +39,7 @@ const logger = new Logger("Service");
  */
 function requestCredentials(): Promise<void> {
   return new Promise<void>((resolve) => {
-    AuthenticationCtrl.setup(process.env.AUTH_SERVER);
+    AuthenticationCtrl.setup(process.env.AUTH_SERVER, process.env.AUTH_PROJECT);
     AuthenticationCtrl.authService(process.env.AUTH_ID, process.env.AUTH_SECRET)
       .catch((err) => {
         logger.error("Invalid authentication configuration", err);
@@ -84,15 +85,16 @@ function runWorker(worker: any): Promise<void> {
 
 /* Run cluster with base routes */
 clusterServer.run({
-  "/v1/domain": DomainRouter,
-  "/v1/project": [
+  "/v1/admin/domain": DomainRouter,
+  "/v1/admin/project": [
     ProjectRouter,
     ProjectExternalAuthRouter,
     ProjectRestrictIpRouter,
     ProjectKeyRouter,
     ProjectSettingsRouter
   ],
-  "/v1/application": ApplicationRouter,
-  "/v1/oauth": [AccountRouter, OAuth2Router],
-  "/v1/oauth/external": ExternalAuthRouter
+  "/v1/admin/application": ApplicationRouter,
+  "/v1/oauth": OAuth2ValidateRouter,
+  "/v1/oauth/:project": [AccountRouter, OAuth2Router],
+  "/v1/oauth/:project/external": ExternalAuthRouter
 });
