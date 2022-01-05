d0b5c14362240c0251c39aad0590697e0c7102a7
Reinier Millo <millo@ikoabo.com>
2022-01-05 00:07:35 -0500

    Add settings interface
    Signed-off-by: Reinier Millo <millo@ikoabo.com>

---
+++ src/settings.ts
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2020-2022 ECUALEAD
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <rmillo@ecualead.com>
+ *
+ * This file is part of the ECUALEAD OAuth2 Server API.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+import { EMAIL_CONFIRMATION, EXTERNAL_AUTH_TYPE, TOKEN_TYPE } from "./constants/project.enum";
+
+export interface IExternalAuth {
+  name: string;
+  type: EXTERNAL_AUTH_TYPE;
+  clientId: string;
+  clientSecret: string;
+  scope?: string;
+  profile?: string[];
+  redirect?: string[];
+}
+
+export interface IPasswordPolicy {
+  ttl?: number;
+  minLen: number;
+  upperCase?: boolean;
+  lowerCase?: boolean;
+  specialChars?: boolean;
+  numbers?: boolean;
+}
+
+export interface ITokenPolicy {
+  accessTokenTtl: number;
+  refreshTokenTtl: number;
+}
+
+export interface ISignKeys {
+  privateKey: string;
+  publicKey: string;
+}
+
+export interface IEmailPolicy {
+  type: EMAIL_CONFIRMATION;
+  ttl: number;
+}
+
+export interface IEmailNotifications {
+  registerEvent?: boolean;
+  confirmEvent?: boolean;
+  loginEvent?: boolean;
+  chPwdEvent?: boolean;
+  recoverEvent?: boolean;
+  token: TOKEN_TYPE;
+}
+
+export interface IOauth2Settings {
+  name: string;
+  version: string;
+
+  passwordPolicy: IPasswordPolicy;
+  tokenPolicy: ITokenPolicy;
+  emailPolicy: IEmailPolicy;
+  handleReferral?: boolean;
+  emailNotifications: IEmailNotifications;
+
+  externalAuth?: IExternalAuth[];
+}
