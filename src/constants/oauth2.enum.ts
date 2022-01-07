7fa22de945fc651a5c2307cc6def979b36d02cc1
Reinier Millo <millo@ikoabo.com>
2022-01-07 15:33:45 -0500

    MOve constants to oauth2.enum
    Signed-off-by: Reinier Millo <millo@ikoabo.com>

--- src/constants/oauth2.enum.ts
+++ src/constants/oauth2.enum.ts
@@ -8,6 +8,17 @@
  * permission of the author.
  */
 
+export enum APPLICATION_TYPE {
+  UNKNOWN = 0,
+  MODULE,
+  SERVICE,
+  ANDROID,
+  IOS,
+  WEB_SERVER_SIDE,
+  WEB_CLIENT_SIDE,
+  MAX
+}
+
 export enum NOTIFICATION_TYPE {
   NONE = 0,
   EMAIL = 1,
@@ -55,3 +66,50 @@ export enum EXTERNAL_AUTH_TYPE {
   TELEGRAM = 7,
   OAUTH2 = 8
 }
+
+export enum ACCOUNT_STATUS {
+  DISABLED_BY_ADMIN = -4,
+  TEMPORALLY_BLOCKED = -3,
+  CANCELLED = -2,
+  DELETED = -1,
+  UNKNOWN = 0,
+  DISABLED = 1,
+  ENABLED = 2,
+  REGISTERED = 3
+}
+
+export enum TOKEN_STATUS {
+  DISABLED = 0,
+  TO_CONFIRM = 1,
+  TO_RECOVER = 2,
+  PARTIAL_CONFIRMED = 3,
+  TO_LOGIN = 4
+}
+
+export enum EVENT_TYPE {
+  UNKNOWN = 0,
+  REGISTER = 1,
+  CONFIRM = 2,
+  LOGIN = 3,
+  CHPWD = 4,
+  RECOVER = 5
+}
+
+export enum VALIDATION_STATUS {
+  DISABLED_BY_ADMIN = -3,
+  TEMPORALLY_BLOCKED = -2,
+  CANCELLED = -1,
+  UNKNOWN = 0,
+  DISABLED = 1,
+  ENABLED = 2,
+  REGISTERED = 3,
+  CONFIRMED = 4,
+  NEEDS_CONFIRM_CAN_NOT_AUTH = 5,
+  NEEDS_CONFIRM_CAN_AUTH = 6
+}
+
+/**
+ * Not allowed scope to be assigned to applications
+ * This are predefined scope assigned internally
+ */
+export const SCOPE_PREVENT: string[] = ["application", "user", "default"];
