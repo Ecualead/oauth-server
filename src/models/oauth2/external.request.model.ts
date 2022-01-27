e84093d0c80a81660d4511b81f01029ea5df5ebf
Reinier Millo <millo@ikoabo.com>
2022-01-27 13:15:21 -0500

    Update external request data model
    Signed-off-by: Reinier Millo <millo@ikoabo.com>

--- src/models/oauth2/external.request.model.ts
+++ src/models/oauth2/external.request.model.ts
@@ -7,39 +7,18 @@
  * It can't be copied and/or distributed without the express
  * permission of the author.
  */
-import { EXTERNAL_AUTH_TYPE } from "@ecualead/auth";
 import { BaseModel } from "@ecualead/server";
 import { prop, Ref, index, getModelForClass, DocumentType } from "@typegoose/typegoose";
 import mongoose from "mongoose";
 import { Account } from "../account/account.model";
 import { Application } from "../application/application.model";
 
-export class ExternalAuthSettings {
-  @prop({ required: true })
-  name!: string;
-
-  @prop({ required: true, enum: EXTERNAL_AUTH_TYPE })
-  type!: EXTERNAL_AUTH_TYPE;
-
-  @prop({ required: true })
-  clientId!: string;
-
-  @prop({ required: true })
-  clientSecret!: string;
-
-  @prop()
-  scope?: string;
-
-  @prop({ type: String })
-  profile?: string[];
-
-  @prop({ type: String })
-  redirect?: string[];
-}
-
 @index({ application: 1 })
 @index({ user: 1 })
 export class ExternalRequest extends BaseModel {
+  @prop({ required: true })
+  external!: string;
+
   @prop({ required: true })
   token!: string;
 
@@ -58,8 +37,8 @@ export class ExternalRequest extends BaseModel {
   @prop()
   type?: number;
 
-  @prop({ required: true })
-  settings!: ExternalAuthSettings;
+  @prop()
+  parent?: string;
 
   /**
    * Get the mongoose data model
@@ -75,16 +54,14 @@ export class ExternalRequest extends BaseModel {
           transform: (_doc: any, ret: any) => {
             return {
               id: ret.id,
+              external: ret.external,
               token: ret.token,
               type: ret.type,
               application: ret.application,
-              user: ret.user,
+              account: ret.account,
               redirect: ret.redirect,
               referral: ret.referral,
-              settings: ret.settings,
-              status: ret.status,
-              createdAt: ret.createdAt,
-              updatedAt: ret.updatedAt
+              parent: ret.parent
             };
           }
         }
